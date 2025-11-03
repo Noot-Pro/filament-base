f4d9d15fe5216267c4234a36ad5e2b914ea42cdb
aymanalhattami <34315778+aymanalhattami@users.noreply.github.com>
2025-11-03 17:41:42 +0000

    Fix styling

--- src/Resource/BaseEditRecord.php
+++ src/Resource/BaseEditRecord.php
@@ -2,15 +2,9 @@
 
 namespace NootPro\FilamentBase\Resource;
 
-use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Placeholder;
-use Filament\Forms\Components\Section;
 use Filament\Resources\Pages\EditRecord;
-use Filament\Support\Enums\MaxWidth;
-use LaraZeus\Popover\Form\PopoverForm;
 use NootPro\FilamentBase\Actions\RecordInformationAction;
 
 class BaseEditRecord extends EditRecord
