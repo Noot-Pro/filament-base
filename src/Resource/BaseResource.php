695c07767e11e03776b553eb5493be302e76ccef
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-03 20:01:38 +0300

    wip: parepare base resource

--- src/Resource/BaseResource.php
+++ src/Resource/BaseResource.php
@@ -4,4 +4,20 @@ namespace NootPro\FilamentBase\Resource;
 
 use Filament\Resources\Resource;
 
-class BaseResource extends Resource {}
+class BaseResource extends Resource
+{
+    public static function langFile(): string
+    {
+        return str(parent::getSlug())->explode('/')->last();
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __(static::langFile().'.titleSingle');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __(static::langFile().'.title');
+    }
+}
