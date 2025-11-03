bd97f4ac71c4a87f558984bcad182c440ed5e6f4
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-03 21:24:36 +0300

    feat: add RecordInformationAction to view page

--- src/Resource/BaseViewRecord.php
+++ src/Resource/BaseViewRecord.php
@@ -2,6 +2,17 @@
 
 namespace NootPro\FilamentBase\Resource;
 
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
+use NootPro\FilamentBase\Actions\RecordInformationAction;
 
-class BaseViewRecord extends ViewRecord {}
+class BaseViewRecord extends ViewRecord
+{
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+            RecordInformationAction::make(),
+        ];
+    }
+}
