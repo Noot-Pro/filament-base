2beaacb15da66ede37a70bc257525b4383cbc6d6
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-04 19:23:50 +0300

    feat: add MultiLang.php form component

---
+++ src/Forms/Components/MultiLang.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace NootPro\FilamentBase\Forms\Components;
+
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\TextInput;
+
+class MultiLang extends Tabs
+{
+    public string $langKey = '';
+
+    public static function make(?string $label = null): static
+    {
+        static::configureUsing(function (self $component) use ($label) {
+            if ($label !== null) {
+                $component->langKey = $label;
+            }
+        });
+
+        return parent::make($label);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->extraAttributes(['class' => 'fi-multi-lang-input'])
+            ->tabs(function (MultiLang $multiLangComponent) {
+                $tabs = [];
+                /** @var array<string, array{name: string}> $locales */
+                $locales = config('custom.locales') ?? ['en' => ['name' => 'English'], 'ar' => ['name' => 'Arabic']];
+
+                foreach ($locales as $lang => $info) {
+                    $langCode = (string) $lang;
+                    $langName = $info['name'];
+
+                    $tabs[] = Tabs\Tab::make('tab-'.$langCode)
+                        ->statePath($this->getLangKey())
+                        ->label($langName)
+                        ->schema(fn (Tabs\Tab $tabComponent) => [
+                            TextInput::make($langCode)
+                                ->required(fn () => app()->getLocale() === $langCode)
+                                ->label(fn () => $multiLangComponent->getLabel()),
+                        ]);
+                }
+
+                return $tabs;
+            });
+    }
+
+    public function getLangKey(): string
+    {
+        return $this->langKey;
+    }
+}
